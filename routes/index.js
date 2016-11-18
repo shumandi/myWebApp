9b11ffdc6aa62730fb51793038f5f33ce51c7ea5
普通ant <shumandi@live.com>
2016-11-18 14:09:59 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.sendFile(path.normalize(__dirname + '/../views') + '/index.html')
 })
-
+router.route('/signup').get((req, res, next) => {
+  res.json(req.user);
+})
 module.exports = router;
